584d4b761ac5fe52b031093b8063e79266108b3c
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-08-26 14:45:54 -0700

    feat: utils

---
+++ src/shared/utils/functions/getCharacter.ts
@@ -0,0 +1,9 @@
+import { promiseR6 } from "@rbxts/promise-character";
+
+export async function getCharacter(player: Player) {
+  const character = await promiseR6(
+    player.Character ?? player.CharacterAdded.Wait()[0],
+  );
+
+  return character;
+}
