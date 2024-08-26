3b6247f13559a8ad9dda42e57cd46b8ebc13cc7d
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-08-26 15:15:05 -0700

    fix: no-any usePx

--- src/client/ui/app.story.tsx
+++ src/client/ui/app.story.tsx
@@ -10,8 +10,6 @@ const Story = {
 	react: React,
 	reactRoblox: ReactRoblox,
 	story: () => {
-		// todo : remove comment below
-		// eslint-disable-next-line roblox-ts/no-any, @typescript-eslint/no-unsafe-call
 		usePx();
 		return <App />;
 	},
