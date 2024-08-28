5a77de6add7952da4a97cf5c856cb5b60752930b
bryson-g <bryson.glembin@gmail.com>
2024-08-27 18:08:43 -0700

    feat: rhthym controls

--- src/client/ui/apps/rhthym/rhthym-app.tsx
+++ src/client/ui/apps/rhthym/rhthym-app.tsx
@@ -1,29 +1,55 @@
-import React, { createContext, useContext, useEffect, useState } from "@rbxts/react";
+import React, { createContext, useContext, useEffect, useReducer, useRef, useState } from "@rbxts/react";
 import { px } from "client/ui/utils/usePx";
 import { UserInputService } from "@rbxts/services";
+import motion from "@rbxts/react-motion";
+import { Janitor } from "@rbxts/janitor";
 
-const KeyCodeContext = createContext<Enum.KeyCode | undefined>(undefined);
+type KeyCodes = Record<string, "active" | "inactive">;
+const KeyCodeContext = createContext<KeyCodes | undefined>(undefined);
 
-export default function RhthymApp() {
-	const [keyCode, setKeyCode] = useState<Enum.KeyCode | undefined>();
+function useKeyboardState() {
+	const [keyCodes, setKeyCodes] = useState({});
 
 	useEffect(() => {
-		UserInputService.InputBegan.Connect((input) => {
-			setKeyCode(input.KeyCode);
-		});
+		const obliterator = new Janitor();
+
+		const handleInputBegan = (input: InputObject, gp: boolean) => {
+			if (input.UserInputType !== Enum.UserInputType.Keyboard) return;
+			setKeyCodes((prev) => ({ ...prev, [input.KeyCode.Name]: "active" }));
+		};
+
+		const handleInputEnded = (input: InputObject, gp: boolean) => {
+			if (input.UserInputType !== Enum.UserInputType.Keyboard) return;
+			setKeyCodes((prev) => ({ ...prev, [input.KeyCode.Name]: "inactive" }));
+		};
+
+		obliterator.Add(UserInputService.InputBegan.Connect(handleInputBegan), "Disconnect");
+		obliterator.Add(UserInputService.InputEnded.Connect(handleInputEnded), "Disconnect");
+		return () => obliterator.Cleanup();
 	}, []);
+
+	return keyCodes;
+}
+
+export default function RhthymApp() {
+	const keyCodes = useKeyboardState();
 
 	return (
 		<>
 			<frame
-				AutomaticSize={"XY"}
-				BackgroundTransparency={1}
+				Size={new UDim2(1, 0, 0, px(200))}
+				BackgroundTransparency={0.5}
 				Position={UDim2.fromScale(0.5, 0.5)}
-				AnchorPoint={new Vector2(0.5, 0.5)}
+				AnchorPoint={new Vector2(0.5, 0)}
 			>
-				<uilistlayout FillDirection={"Horizontal"} Padding={new UDim(0.25, 0)} />
+				<uilistlayout
+					FillDirection={"Horizontal"}
+					Padding={new UDim(0, px(100))}
+					VerticalAlignment={"Center"}
+					HorizontalAlignment={"Center"}
+				/>
 
-				<KeyCodeContext.Provider value={keyCode}>
+				<KeyCodeContext.Provider value={keyCodes}>
 					<KeyCircle keyCode={Enum.KeyCode.A} color={Color3.fromRGB(217, 79, 79)} />
 					<KeyCircle keyCode={Enum.KeyCode.S} color={Color3.fromRGB(204, 186, 79)} />
 					<KeyCircle keyCode={Enum.KeyCode.K} color={Color3.fromRGB(79, 204, 82)} />
@@ -35,15 +61,28 @@ export default function RhthymApp() {
 }
 
 function KeyCircle({ keyCode, color }: { keyCode: Enum.KeyCode; color: Color3 }) {
-	const pressedKeyCode = useContext(KeyCodeContext);
-	print(pressedKeyCode, pressedKeyCode === keyCode);
+	const keyCodes = useContext(KeyCodeContext);
+	if (keyCodes === undefined) return;
 
 	return (
-		<frame
+		<motion.frame
+			animate={keyCodes[keyCode.Name] ?? "inactive"}
+			initial={"inactive"}
 			Size={UDim2.fromOffset(px(100), px(100))}
 			BorderColor3={Color3.fromRGB(0, 0, 0)}
 			BorderSizePixel={3}
 			BackgroundColor3={color}
+			transition={{ duration: 0.1, easingStyle: "Cubic" }}
+			variants={{
+				active: {
+					Size: UDim2.fromOffset(px(120), px(120)),
+					BackgroundColor3: Color3.fromRGB(255, 255, 255),
+				},
+				inactive: {
+					Size: UDim2.fromOffset(px(100), px(100)),
+					BackgroundColor3: color,
+				},
+			}}
 		>
 			<uicorner CornerRadius={new UDim(1, 0)} />
 			<uistroke ApplyStrokeMode={Enum.ApplyStrokeMode.Border} Thickness={px(6)} />
@@ -55,6 +94,6 @@ function KeyCircle({ keyCode, color }: { keyCode: Enum.KeyCode; color: Color3 })
 				TextColor3={Color3.fromRGB(0, 0, 0)}
 				Font={"ArialBold"}
 			/>
-		</frame>
+		</motion.frame>
 	);
 }
