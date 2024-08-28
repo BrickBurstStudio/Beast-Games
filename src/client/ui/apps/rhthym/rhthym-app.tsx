bc4db2ab8dc2e74142c9fc1726eba2e4032e3ca0
bryson-g <bryson.glembin@gmail.com>
2024-08-27 19:18:37 -0700

    feat(wip): notes

--- src/client/ui/apps/rhthym/rhthym-app.tsx
+++ src/client/ui/apps/rhthym/rhthym-app.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useReducer, useRef, useState } from "@rbxts/react";
+import React, { createContext, useContext, useEffect, useRef, useState } from "@rbxts/react";
 import { px } from "client/ui/utils/usePx";
 import { UserInputService } from "@rbxts/services";
 import motion from "@rbxts/react-motion";
@@ -6,6 +6,28 @@ import { Janitor } from "@rbxts/janitor";
 
 type KeyCodes = Record<string, "active" | "inactive">;
 const KeyCodeContext = createContext<KeyCodes | undefined>(undefined);
+
+const notes: Array<{ delay: number; keyCode: Enum.KeyCode }> = [
+	{ delay: 1.5, keyCode: Enum.KeyCode.A },
+	{ delay: 2, keyCode: Enum.KeyCode.A },
+	{ delay: 2.5, keyCode: Enum.KeyCode.A },
+	{ delay: 3.5, keyCode: Enum.KeyCode.A },
+];
+
+const AnimatedNote = React.memo(
+	({ n, aControlRef }: { n: typeof notes[number]; aControlRef: React.MutableRefObject<Frame | undefined> }) =>
+		aControlRef.current && (
+			<motion.frame
+				BackgroundTransparency={0}
+				Size={UDim2.fromOffset(px(100), px(100))}
+				Position={new UDim2(0, aControlRef.current?.AbsolutePosition.X, -0.25, 0)}
+				animate={{
+					Position: new UDim2(0, aControlRef.current?.AbsolutePosition.X, 1.25, 0),
+				}}
+				transition={{ easingStyle: "Linear", delay: n.delay }}
+			/>
+		),
+);
 
 function useKeyboardState() {
 	const [keyCodes, setKeyCodes] = useState({});
@@ -33,9 +55,15 @@ function useKeyboardState() {
 
 export default function RhthymApp() {
 	const keyCodes = useKeyboardState();
+	const aControlRef = useRef<Frame | undefined>();
+	const sControlRef = useRef<Frame | undefined>();
+	const kControlRef = useRef<Frame | undefined>();
+	const lControlRef = useRef<Frame | undefined>();
 
 	return (
 		<>
+			{aControlRef.current && notes.map((n, i) => <AnimatedNote key={i} n={n} aControlRef={aControlRef} />)}
+
 			<frame
 				Size={new UDim2(1, 0, 0, px(200))}
 				BackgroundTransparency={0.5}
@@ -50,22 +78,31 @@ export default function RhthymApp() {
 				/>
 
 				<KeyCodeContext.Provider value={keyCodes}>
-					<KeyCircle keyCode={Enum.KeyCode.A} color={Color3.fromRGB(217, 79, 79)} />
-					<KeyCircle keyCode={Enum.KeyCode.S} color={Color3.fromRGB(204, 186, 79)} />
-					<KeyCircle keyCode={Enum.KeyCode.K} color={Color3.fromRGB(79, 204, 82)} />
-					<KeyCircle keyCode={Enum.KeyCode.L} color={Color3.fromRGB(89, 79, 204)} />
+					<KeyCircle controlRef={aControlRef} keyCode={Enum.KeyCode.A} color={Color3.fromRGB(217, 79, 79)} />
+					<KeyCircle controlRef={sControlRef} keyCode={Enum.KeyCode.S} color={Color3.fromRGB(204, 186, 79)} />
+					<KeyCircle controlRef={kControlRef} keyCode={Enum.KeyCode.K} color={Color3.fromRGB(79, 204, 82)} />
+					<KeyCircle controlRef={lControlRef} keyCode={Enum.KeyCode.L} color={Color3.fromRGB(89, 79, 204)} />
 				</KeyCodeContext.Provider>
 			</frame>
 		</>
 	);
 }
 
-function KeyCircle({ keyCode, color }: { keyCode: Enum.KeyCode; color: Color3 }) {
+function KeyCircle({
+	keyCode,
+	color,
+	controlRef,
+}: {
+	keyCode: Enum.KeyCode;
+	color: Color3;
+	controlRef: React.MutableRefObject<Frame | undefined>;
+}) {
 	const keyCodes = useContext(KeyCodeContext);
 	if (keyCodes === undefined) return;
 
 	return (
 		<motion.frame
+			ref={controlRef}
 			animate={keyCodes[keyCode.Name] ?? "inactive"}
 			initial={"inactive"}
 			Size={UDim2.fromOffset(px(100), px(100))}
@@ -75,24 +112,35 @@ function KeyCircle({ keyCode, color }: { keyCode: Enum.KeyCode; color: Color3 })
 			transition={{ duration: 0.1, easingStyle: "Cubic" }}
 			variants={{
 				active: {
-					Size: UDim2.fromOffset(px(120), px(120)),
+					// Size: UDim2.fromOffset(px(120), px(120)),
 					BackgroundColor3: Color3.fromRGB(255, 255, 255),
 				},
 				inactive: {
-					Size: UDim2.fromOffset(px(100), px(100)),
+					// Size: UDim2.fromOffset(px(100), px(100)),
 					BackgroundColor3: color,
 				},
 			}}
 		>
 			<uicorner CornerRadius={new UDim(1, 0)} />
 			<uistroke ApplyStrokeMode={Enum.ApplyStrokeMode.Border} Thickness={px(6)} />
-			<textbutton
+			<motion.textbutton
+				animate={keyCodes[keyCode.Name] ?? "inactive"}
+				initial={"inactive"}
 				BackgroundTransparency={1}
 				Size={UDim2.fromScale(1, 1)}
 				Text={keyCode.Name}
 				TextSize={px(80)}
 				TextColor3={Color3.fromRGB(0, 0, 0)}
 				Font={"ArialBold"}
+				transition={{ duration: 0.1, easingStyle: "Cubic" }}
+				variants={{
+					active: {
+						TextColor3: color,
+					},
+					inactive: {
+						TextColor3: Color3.fromRGB(0, 0, 0),
+					},
+				}}
 			/>
 		</motion.frame>
 	);
