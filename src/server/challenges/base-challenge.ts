f573d2000556df36d2cf730318abdc4327b4f587
bryson-g <bryson.glembin@gmail.com>
2024-09-03 19:34:29 -0700

    feat: protected obliterator

--- src/server/challenges/base-challenge.ts
+++ src/server/challenges/base-challenge.ts
@@ -1,17 +1,21 @@
 import { Components } from "@flamework/components";
 import { Service } from "@flamework/core";
+import { Janitor } from "@rbxts/janitor";
 import Make from "@rbxts/make";
 import { Players, ServerStorage, Workspace } from "@rbxts/services";
 import { getCharacter } from "shared/utils/functions/getCharacter";
 
 export abstract class BaseChallenge {
 	private finished = false;
+	protected obliterator = new Janitor();
 	protected abstract readonly map: Folder;
 
 	public Start() {
+		this.obliterator.Add(this.map, "Destroy");
 		this.map.Parent = Workspace;
 		this.Main();
 		while (!this.finished) task.wait();
+		this.obliterator.Cleanup();
 	}
 
 	protected abstract Main(): void;
@@ -22,6 +26,4 @@ export abstract class BaseChallenge {
 		const character = await getCharacter(player);
 		character.Humanoid.Health = 0;
 	}
-
-	// private;
 }
