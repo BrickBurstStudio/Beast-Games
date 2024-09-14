77bcc93208552ca1f1090befb13659c33985bae7
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-09-13 19:40:28 -0700

    refactor: remove test

--- src/server/services/game-service.ts
+++ src/server/services/game-service.ts
@@ -7,12 +7,5 @@ class GameService implements OnStart {
 	onStart() {
 		while (Players.GetPlayers().size() < 1) task.wait();
 		// new BriefcaseChallenge().Start();
-
-		task.wait(3);
-		const player = Players.GetPlayers()[0];
-		store.equip(tostring(player.UserId), "emote_1");
 	}
 }
-
-type ValidItem = `${"emote" | "hat"}_${number}`;
-const x: ValidItem = "emote_1";
