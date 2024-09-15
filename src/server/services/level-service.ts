6c5aeca0725591231644e3c363e5fd8b049649bb
Hazim Arafa <hazimarafa80@gmail.com>
2024-09-15 11:59:41 -0700

    feat: initial distribution rewards (#6)

--- src/server/services/level-service.ts
+++ src/server/services/level-service.ts
@@ -1,13 +1,33 @@
 import { OnStart, Service } from "@flamework/core";
-import { Functions } from "server/network";
 import { store } from "server/store";
-import { items } from "shared/configs/items";
-import { cases } from "shared/configs/items/cases";
-import { selectPlayerBalance } from "shared/store/selectors/players";
+import { selectPlayerXP } from "shared/store/selectors/players";
+import { forEveryPlayer } from "shared/utils/functions/forEveryPlayer";
+import { getLevel } from "shared/utils/functions/getLevel";
 
 @Service()
 export class LevelService implements OnStart {
 	onStart() {
-		
+		forEveryPlayer((player) => {
+			task.wait(3);
+			let previousLevel: number | undefined = undefined;
+			store.once(selectPlayerXP(tostring(player.UserId)), (xp) => {
+				if (!xp) return;
+				previousLevel = getLevel(xp);
+			});
+			store.subscribe(selectPlayerXP(tostring(player.UserId)), (xp) => {
+				if (previousLevel === undefined) return;
+				if (!xp) return;
+				const currentLevel = getLevel(xp);
+				if (currentLevel > previousLevel) {
+					this.distributeRewards(player);
+					previousLevel = currentLevel;
+				}
+			});
+		});
+	}
+
+	distributeRewards(player: Player) {
+		store.changeBalance(tostring(player.UserId), "cash", 100_000);
+		store.changeBalance(tostring(player.UserId), "gems", 100);
 	}
 }
