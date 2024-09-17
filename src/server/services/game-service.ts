858e71eb9ac32ab4ece0a8322145419e8ba3b40b
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-09-16 20:26:18 -0700

    feat: dares

--- src/server/services/game-service.ts
+++ src/server/services/game-service.ts
@@ -1,11 +1,24 @@
 import { OnStart, Service } from "@flamework/core";
 import { Players } from "@rbxts/services";
 import { store } from "server/store";
+import { createDare } from "server/util/createDare";
 
 @Service()
 export class GameService implements OnStart {
 	onStart() {
 		while (Players.GetPlayers().size() < 1) task.wait();
 		// new BriefcaseChallenge().Start();
+		const completeTarget = createDare(
+			{
+				name: "",
+				description: "",
+				reward: "cash",
+				targets: 1,
+			},
+			(player) => {
+				print("reward some shit");
+			},
+		);
+		completeTarget(Players.GetPlayers()[0]);
 	}
 }
