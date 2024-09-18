bd839352ae7673cb2485295fc8335242ef076115
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-09-18 13:07:04 -0700

    refactor: task.wait -> store.once

--- src/client/controllers/gui-controller.tsx
+++ src/client/controllers/gui-controller.tsx
@@ -2,28 +2,30 @@ import { Controller, OnStart } from "@flamework/core";
 import React from "@rbxts/react";
 import { createPortal, createRoot } from "@rbxts/react-roblox";
 import { Players } from "@rbxts/services";
+import { store } from "client/store";
 import App from "client/ui/apps/app";
 import setupLeaderboards from "client/ui/apps/leaderboards";
 import { usePx } from "client/ui/utils/usePx";
+import { selectPlayerData } from "shared/store/selectors/players";
 
 @Controller()
 export class GuiController implements OnStart {
 	private playerGui = Players.LocalPlayer.WaitForChild("PlayerGui");
 
 	onStart() {
-		task.wait(3);
+		store.once(selectPlayerData(tostring(Players.LocalPlayer.UserId)), () => {
+			usePx();
+			const root = createRoot(new Instance("Folder"));
+			root.render(
+				createPortal(
+					<screengui Enabled={true} ResetOnSpawn={false}>
+						<App />
+					</screengui>,
+					this.playerGui,
+				),
+			);
 
-		usePx();
-		const root = createRoot(new Instance("Folder"));
-		root.render(
-			createPortal(
-				<screengui Enabled={true} ResetOnSpawn={false}>
-					<App />
-				</screengui>,
-				this.playerGui,
-			),
-		);
-
-		setupLeaderboards();
+			setupLeaderboards();
+		});
 	}
 }
