6495f3b8f888f2da6338c9a47131e29d06ca54bf
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-09-18 18:45:47 -0700

    feat: announcer

--- src/client/ui/apps/announcer/announcer-app.tsx
+++ src/client/ui/apps/announcer/announcer-app.tsx
@@ -1,18 +1,39 @@
-import React from "@rbxts/react";
+import React, { useEffect, useRef, useState } from "@rbxts/react";
 import motion from "@rbxts/react-motion";
+import { Events } from "client/network";
 import { px } from "client/ui/utils/usePx";
 
+function useAnnouncement() {
+	const [message, setMessage] = useState<string | undefined>();
+	const lastMessage = useRef("_!_");
+
+	useEffect(() => {
+		const conn = Events.announcer.announce.connect((announcement) => {
+			setMessage(announcement.message);
+			lastMessage.current = announcement.message;
+			task.wait(5);
+			if (lastMessage.current === announcement.message) setMessage(undefined);
+		});
+
+		return () => conn.Disconnect();
+	}, []);
+
+	return message;
+}
+
 export default function AnnouncerApp() {
+	const message = useAnnouncement();
+
 	return (
 		<motion.frame
 			BackgroundTransparency={1}
 			Size={UDim2.fromOffset(px(1000), px(200))}
-			Position={UDim2.fromScale(0.5, 0.5)}
-			AnchorPoint={new Vector2(0.5, 0)}
+			Position={UDim2.fromScale(0.5, 0.8)}
+			AnchorPoint={new Vector2(0.5, 1)}
 		>
 			<textlabel
 				Size={UDim2.fromScale(1, 1)}
-				Text={"Welcome to Beast Games! You will be participating to win $5,000,000!"}
+				Text={message ?? ""}
 				TextColor3={Color3.fromRGB(255, 255, 255)}
 				TextSize={30}
 				Font={"Jura"}
