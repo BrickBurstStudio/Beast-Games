956e25ecbebcce7dea714841c7ce7d5f26b5deb6
Hazim Arafa <hazimarafa80@gmail.com>
2024-09-22 10:36:14 -0700

    feat: continue UI changes

---
+++ src/client/ui/apps/menu/pages/inventory.tsx
@@ -0,0 +1,6 @@
+import React from "@rbxts/react";
+import MenuFrame from "client/ui/components/menu-frame";
+
+export default function InventoryApp() {
+	return <MenuFrame title="Inventory" />;
+}
---
+++ src/client/ui/apps/menu/pages/trading.tsx
@@ -0,0 +1,6 @@
+import React from "@rbxts/react";
+import MenuFrame from "client/ui/components/menu-frame";
+
+export default function TradingApp() {
+	return <MenuFrame title="Trading" square />;
+}
