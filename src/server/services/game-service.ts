9701f7db5102f2c600f40b0ec707e91e3ec79157
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-09-27 20:37:28 -0700

    Update game-service.ts

--- src/server/services/game-service.ts
+++ src/server/services/game-service.ts
@@ -3,11 +3,12 @@ import { MarketplaceService, Players } from "@rbxts/services";
 import { ProductService } from "./product-service";
 import { BriefcaseChallenge } from "server/challenges/briefcase-challenge";
 import { announceAndWait } from "server/util/announceAndWait";
+import { MoneyPileChallenge } from "server/challenges/money-pile-challenge";
 
 @Service()
 export class GameService implements OnStart {
 	async onStart() {
 		while (Players.GetPlayers().size() < 1) task.wait();
-		await new BriefcaseChallenge().Start();
+		await new MoneyPileChallenge().Start();
 	}
 }
