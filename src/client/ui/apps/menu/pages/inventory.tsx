cf224e54874feeb53e9920ef9eb9d643bd377a48
Hazim Arafa <hazimarafa80@gmail.com>
2024-09-30 17:02:54 -0700

    feat: animate client events

--- src/client/ui/apps/menu/pages/inventory.tsx
+++ src/client/ui/apps/menu/pages/inventory.tsx
@@ -56,27 +56,10 @@ export default function InventoryApp() {
 						CellPadding={new UDim2(0, px(15), 0, px(15))}
 						CellSize={new UDim2(0, px(150), 0, px(150))}
 					/>
-					{/* {itemIds.map((itemId) => (
-						<ImageButton
-							key={itemId}
-							image={"rbxassetid://3926305904"}
-							onClick={() => {
-								store.setGuiPage(undefined);
-								if (isCase) Functions.inventory.openCase(itemId as (typeof cases)[number]["id"]);
-								else Functions.inventory.equip(itemId as EquippableItemId);
-							}}
-							toolTip={{
-								header: items.get(itemId)?.name || "THIS SHOULDNT HAPPEN. PLEASE REPORT BUG TO DEVS",
-								body: `Click To ${isCase ? "Open" : "Equip"}`,
-							}}
-						/>
-					))} */}
 					{itemMapObjects.map((itemMapObject) => {
 						const item = items.get(itemMapObject.id);
-						// show the quantity of the item on top of the image
-						print(itemMapObject);
 						return (
-							<frame>
+							<frame BackgroundTransparency={0}>
 								<ImageButton
 									key={itemMapObject.id}
 									image={""}
@@ -92,6 +75,7 @@ export default function InventoryApp() {
 										header: item?.name || "THIS SHOULDNT HAPPEN. PLEASE REPORT BUG TO DEVS",
 										body: `Click To ${isCase ? "Open" : "Equip"}`,
 									}}
+									size={UDim2.fromScale(1, 1)}
 								/>
 								{itemMapObject.quantity > 1 && (
 									<textlabel
