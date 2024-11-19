65328ba5dd0ec1de7541a969ff01fcce1e97b69f
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-19 10:06:58 -0800

    fix: darkness service

--- src/server/services/darkness.service.ts
+++ src/server/services/darkness.service.ts
@@ -1,14 +1,11 @@
 import { OnStart, Service } from "@flamework/core";
+import { CharacterRigR6 } from "@rbxts/promise-character";
 import { CollectionService, Players } from "@rbxts/services";
 import { Events } from "server/network";
 
 @Service()
-export class RandomAssFuckinServiceForFuckAssActivities implements OnStart {
+export class Darkness implements OnStart {
 	onStart() {
-		this.setupDarknessHitbox();
-	}
-
-	setupDarknessHitbox() {
 		CollectionService.GetTagged("darkness-hitbox").forEach((hitbox) => {
 			if (!hitbox.IsA("BasePart")) return;
 
@@ -17,6 +14,8 @@ export class RandomAssFuckinServiceForFuckAssActivities implements OnStart {
 				if (!player) return;
 
 				Events.animations.setBlackFade(player, true);
+				task.wait(3);
+				(hit.Parent as CharacterRigR6).Humanoid.Health = 0;
 			});
 		});
 	}
