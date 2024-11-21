9a0444c90ecac209af6478f237e92e46ba7c4584
Hazim Arafa <hazimoarafa@gmail.com>
2024-11-21 12:00:40 -0800

    fix: move ui to left

--- src/client/ui/apps/menu/buttons.tsx
+++ src/client/ui/apps/menu/buttons.tsx
@@ -6,7 +6,7 @@ import { GuiPage } from "shared/configs/gui";
 
 const BUTTONS: GuiPage[] = ["Inventory", "Shop", "Achievements", "Trading", "Settings"];
 const BUTTON_SIZE = 100;
-const PADDING_SIZE = BUTTON_SIZE / 4;
+const PADDING_SIZE = BUTTON_SIZE / 8;
 
 export default function MenuButtonsApp() {
 	return (
@@ -14,18 +14,18 @@ export default function MenuButtonsApp() {
 			Size={
 				new UDim2(
 					0,
-					px(BUTTON_SIZE) * BUTTONS.size() + px(PADDING_SIZE) * (BUTTONS.size() - 1),
-					0,
 					px(BUTTON_SIZE),
+					0,
+					px(BUTTON_SIZE) * BUTTONS.size() + px(PADDING_SIZE) * (BUTTONS.size() - 1),
 				)
 			}
-			Position={new UDim2(0.5, 0, 1, px(-(BUTTON_SIZE + PADDING_SIZE)))}
-			AnchorPoint={new Vector2(0.5, 0)}
+			Position={new UDim2(0, px(PADDING_SIZE), 0.5, 0)}
+			AnchorPoint={new Vector2(0, 0.5)}
 			BackgroundTransparency={1}
 		>
 			<uigridlayout
-				FillDirection={Enum.FillDirection.Horizontal}
-				CellPadding={new UDim2(0, px(PADDING_SIZE), 0, 0)}
+				FillDirection={Enum.FillDirection.Vertical}
+				CellPadding={new UDim2(0, 0, 0, px(PADDING_SIZE))}
 				CellSize={new UDim2(0, px(BUTTON_SIZE), 0, px(BUTTON_SIZE))}
 			/>
 			{BUTTONS.map((button) => {
