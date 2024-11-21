74490b32d5b48affe3f43a1f6fbe7d74a70a911b
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-20 23:39:41 -0800

    refactor: rename

--- src/server/services/main.service.ts
+++ src/server/services/main.service.ts
@@ -1,7 +1,7 @@
 import { OnInit, OnStart, Service } from "@flamework/core";
 import Log from "@rbxts/log";
 import { CharacterRigR6 } from "@rbxts/promise-character";
-import { Players, StarterGui } from "@rbxts/services";
+import { Players, RunService, StarterGui, TeleportService } from "@rbxts/services";
 import { BoulderChallenge } from "server/challenges/boulder.challenge";
 import { BriefcaseChallenge } from "server/challenges/briefcase.challenge";
 import { GoldRushChallenge } from "server/challenges/gold-rush.challenge";
@@ -14,17 +14,21 @@ import { forEveryPlayer } from "shared/utils/functions/forEveryPlayer";
 import { getCharacter } from "shared/utils/functions/getCharacter";
 
 @Service()
-export class GameService implements OnStart {
+export class MainService implements OnStart {
 	public static DESTROY_CHARACTER_DELAY = 3;
+	private static EXPECTED_PLAYERS = 1;
 
 	async onStart() {
 		if (game.PlaceId !== MAIN_PLACE_ID) return;
-
-		Log.Info("GameService started");
 		this.setupReset();
 		this.setupDestroyCharacterOnDeath();
 
-		while (Players.GetPlayers().size() < 1) task.wait();
+		if (RunService.IsStudio()) {
+			while (Players.GetPlayers().size() < MainService.EXPECTED_PLAYERS) task.wait();
+		} else {
+			// todo: wait for all expected players to join
+			// ? use teleport service networking info somehow
+		}
 		await new PugilChallenge().Start();
 	}
 
