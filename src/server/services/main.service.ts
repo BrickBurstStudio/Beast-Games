1a819d12117ee88c7d9463c76a8d171c86bd68f7
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-21 00:01:30 -0800

    feat: wait for players

--- src/server/services/main.service.ts
+++ src/server/services/main.service.ts
@@ -2,6 +2,7 @@ import { OnInit, OnStart, Service } from "@flamework/core";
 import Log from "@rbxts/log";
 import { CharacterRigR6 } from "@rbxts/promise-character";
 import { Players, RunService, StarterGui, TeleportService } from "@rbxts/services";
+import { setTimeout } from "@rbxts/set-timeout";
 import { BoulderChallenge } from "server/challenges/boulder.challenge";
 import { BriefcaseChallenge } from "server/challenges/briefcase.challenge";
 import { GoldRushChallenge } from "server/challenges/gold-rush.challenge";
@@ -16,20 +17,33 @@ import { getCharacter } from "shared/utils/functions/getCharacter";
 @Service()
 export class MainService implements OnStart {
 	public static DESTROY_CHARACTER_DELAY = 3;
-	private static EXPECTED_PLAYERS = 1;
+	private static EXPECTED_PLAYERS_DEFAULT = 1;
+	private static JOIN_TIMEOUT = 15;
+
+	private playersJoined = 0;
+	private expectedPlayers = MainService.EXPECTED_PLAYERS_DEFAULT;
+	private joinTimedOut = false;
 
 	async onStart() {
 		if (game.PlaceId !== MAIN_PLACE_ID) return;
 		this.setupReset();
 		this.setupDestroyCharacterOnDeath();
+		this.yieldPlayers();
 
-		if (RunService.IsStudio()) {
-			while (Players.GetPlayers().size() < MainService.EXPECTED_PLAYERS) task.wait();
-		} else {
-			// todo: wait for all expected players to join
-			// ? use teleport service networking info somehow
-		}
 		await new PugilChallenge().Start();
+	}
+
+	yieldPlayers() {
+		Players.PlayerAdded.Connect((player) => {
+			this.playersJoined++;
+			this.expectedPlayers = player.GetJoinData().Members?.size() ?? MainService.EXPECTED_PLAYERS_DEFAULT;
+			print(this.expectedPlayers);
+		});
+		setTimeout(() => {
+			this.joinTimedOut = true;
+		}, MainService.JOIN_TIMEOUT);
+
+		while (this.playersJoined < this.expectedPlayers && !this.joinTimedOut) task.wait();
 	}
 
 	setupReset() {
