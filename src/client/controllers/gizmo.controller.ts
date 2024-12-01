edf103cd21b9b020949e485b4350129906efb7b5
Bryson G. <114206517+bryson-g@users.noreply.github.com>
2024-11-30 16:00:19 -0800

    fix: mobile gizmo support

--- src/client/controllers/gizmo.controller.ts
+++ src/client/controllers/gizmo.controller.ts
@@ -6,25 +6,26 @@ import { InputData } from "shared/network";
 @Controller()
 export class GizmoController implements OnStart {
 	onStart() {
-		UserInputService.InputBegan.Connect((input) => {
-			if (input.UserInputType === Enum.UserInputType.MouseButton1) {
-				const mouse = Players.LocalPlayer.GetMouse();
+		if (!UserInputService.TouchEnabled) {
+			UserInputService.InputBegan.Connect((input) => {
+				if (input.UserInputType === Enum.UserInputType.MouseButton1) {
+					const mouse = Players.LocalPlayer.GetMouse();
+
+					Events.inputActivated.fire({
+						origin: mouse.UnitRay.Origin,
+						direction: mouse.UnitRay.Direction,
+					});
+				}
+			});
+		} else {
+			UserInputService.TouchTapInWorld.Connect((position) => {
+				const ray = Workspace.CurrentCamera!.ViewportPointToRay(position.X, position.Y);
 
 				Events.inputActivated.fire({
-					origin: mouse.UnitRay.Origin,
-					direction: mouse.UnitRay.Direction,
+					origin: ray.Origin,
+					direction: ray.Direction,
 				});
-			}
-		});
-
-		UserInputService.TouchTapInWorld.Connect((position) => {
-			print(position);
-			const ray = Workspace.CurrentCamera!.ViewportPointToRay(position.X, position.Y);
-
-			Events.inputActivated.fire({
-				origin: ray.Origin,
-				direction: ray.Direction,
 			});
-		});
+		}
 	}
 }
